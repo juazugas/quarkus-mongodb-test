16496d721c7ce19e04458f3256aec093edb544c0
jzuriaga <jzuriaga@redhat.com>
2019-12-17 12:49:33 +0100

    Add Test with Mongo access.

--- src/test/java/org/acme/mongodb/panache/FruitResourceTest.java
+++ src/test/java/org/acme/mongodb/panache/FruitResourceTest.java
@@ -1,21 +1,40 @@
 package org.acme.mongodb.panache;
 
+import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.mongodb.MongoDatabaseTestResource;
+import io.restassured.http.ContentType;
+
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 @QuarkusTest
+@QuarkusTestResource(MongoDatabaseTestResource.class)
 public class FruitResourceTest {
+
+    private static final String FRUIT_NAME = "fruit";
 
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/fruits")
+          .when().get("/fruits/hello")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(equalTo("hello"));
+    }
+
+    @Test
+    public void testCreateEndpoint() {
+        given()
+          .when()
+          .body("{\"name\":\""+FRUIT_NAME+"\"}")
+          .contentType(ContentType.JSON)
+          .post("/fruits")
+          .then()
+             .statusCode(200)
+             .body("name", equalTo(FRUIT_NAME));
     }
 
 }
